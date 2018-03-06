7594ae2e3e032617051d98c7952165a7c6a582fe
Алексей Бульдович <a.buldovich@betinvest.com>
2018-03-06 18:19:26 +0200

    Erlang homework lesson 02

--- 02/p09.erl
+++ 02/p09.erl
@@ -6,20 +6,16 @@ pack([]) ->
     [];
     
 pack([H|[]]) ->
-   %io:format("pack([H|[]]): H=~p; ~n", [H]),
    [H];
 
 pack([H|T]) ->
     pack(T, [H], []).
 
 pack([], Temp, Acc) ->
-    %io:format("pack([], Temp, Acc): Temp=~p; Acc=~p; ~n", [Temp,Acc]),
     lists:reverse([Temp|Acc]);
 
 pack([H|T], Temp, Acc) when H == hd(Temp) ->
-    %io:format("~n pack([H|T], Temp, Acc) when H == hd(Temp): H=~p; T=~p; Temp=~p; Acc=~p; ~n",[H,T,Temp,Acc]),
     pack(T, [H|Temp], Acc);
 
 pack([H|T], Temp, Acc) ->    
-    %io:format("~n pack([H|T], Temp, Acc): H=~p; T=~p; Temp=~p; Acc=~p; ~n", [H,T,Temp,Acc]),
     pack(T, [H], [Temp|Acc]).
