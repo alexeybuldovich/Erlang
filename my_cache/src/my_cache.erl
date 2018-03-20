68e190f0903af07b1e38c55f631ab0761aefb4dd
Алексей Бульдович <a.buldovich@betinvest.com>
2018-03-20 11:38:10 +0200

    Update

--- my_cache/src/my_cache.erl
+++ my_cache/src/my_cache.erl
@@ -1,5 +1,6 @@
 -module(my_cache).
 -export([create/0, insert/3, lookup/1, delete/0, get_timestamp/0]).
+-include_lib("eunit/include/eunit.hrl").
 
 create() ->
     ets:new(table1, [public, named_table]).
@@ -13,13 +14,22 @@ lookup(Key) ->
     
     Record = ets:lookup(table1, Key),
 
-    [{Key, Value, TimeExpire}] = Record,
+    case Record =:= [] of
+    
+        false -> 
 
-    case TimeExpire >= CurrentTime of 
-        true ->
-            Value;
-        false ->
-            io:format("~n Time is out for current key ~n")
+            [{Key, Value, TimeExpire}] = Record,
+
+            case TimeExpire >= CurrentTime of 
+                true ->
+                    Value;
+                false ->
+                    io:format("~n Time is out for current key ~n")
+            end;
+
+        true -> 
+            io:format("Key is not found")
+        
     end.
     
 
@@ -29,3 +39,12 @@ delete() ->
 get_timestamp() ->                                                                                                                                                                                   
   {Mega, Seconds, MilliSeconds} = erlang:timestamp(),
   (Mega*1000000 + Seconds)*1000 + erlang:round(MilliSeconds/1000).
+
+lookup_test_() ->
+    [
+        ?_assert(create() =:= table1),
+        ?_assert(insert(key1, value1, 600) =:= true),
+        ?_assert(lookup(key1) =:= value1),
+        ?_assert(lookup(key3) =:= <<"Key is not found">>),
+        ?_assert(delete() =:= true)
+    ].
