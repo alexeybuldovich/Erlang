2e7d30c8b3fe58ee8acff8576ebfecefe3ecdaa9
Алексей Бульдович <a.buldovich@betinvest.com>
2018-03-29 17:01:48 +0300

    Lesson 5

--- my_cache/src/my_cache.erl
+++ my_cache/src/my_cache.erl
@@ -1,18 +1,19 @@
 -module(my_cache).
 -export([create/0, insert/3, lookup/1, delete/0, get_timestamp/0]).
 -include_lib("eunit/include/eunit.hrl").
+-define(TABLE_NAME, table1).
 
 create() ->
     ets:new(table1, [public, named_table]).
     
 insert(Key, Value, Time) ->
     TimeExpire = get_timestamp() + Time * 1000,
-    ets:insert(table1, {Key, Value, TimeExpire}).
+    ets:insert(?TABLE_NAME, {Key, Value, TimeExpire}).
     
 lookup(Key) ->
     CurrentTime = get_timestamp(),
     
-    Record = ets:lookup(table1, Key),
+    Record = ets:lookup(?TABLE_NAME, Key),
 
     case Record =:= [] of
     
@@ -28,13 +29,14 @@ lookup(Key) ->
             end;
 
         true -> 
-            io:format("~n Key is not found ~n");
+            io:format("~n Key is not found ~n")
         
     end.
     
 
 delete() ->
-    ets:delete(table1).
+    CurrentTime = get_timestamp(),
+    ets:select_delete(?TABLE_NAME, [{{'$1', '$2', '$3'}, [{'<', '$3', CurrentTime}], [true]}]).
     
 get_timestamp() ->                                                                                                                                                                                   
   {Mega, Seconds, MilliSeconds} = erlang:timestamp(),
