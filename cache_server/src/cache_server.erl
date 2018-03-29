b893d33f958694ba29f50b7e28b55dafc6482ecd
Алексей Бульдович <a.buldovich@betinvest.com>
2018-03-29 15:47:57 +0300

    Lesson 6

--- cache_server/src/cache_server.erl
+++ cache_server/src/cache_server.erl
@@ -8,19 +8,9 @@ start_link([{DropInterval, Interval}]) ->
     TimeExpire = get_timestamp() + Interval * 1000,
 	Pid = spawn(cache_server, loop, [TimeExpire]),
 
-    %register(?SERVER, Pid),
-
     register(?PID, Pid),
-    %register(?DROPINTERVAL, Pid),
 
-	%Pid ! {self(), hello},
     Pid ! {start, self()},
-	%receive
-	%	{Pid, Msg} ->
-    %		io:format("P1 ~w~n",[Msg]);
-    %    {Msg} ->
-    %        io:format("~n Receive ~w~n", [Msg])
-	%end,
 
     get_response(),
 
@@ -30,7 +20,6 @@ start_link([{DropInterval, Interval}]) ->
 
 
     {ok, Pid}.
-	%Pid ! stop.
 
 
 
@@ -53,9 +42,6 @@ lookup_by_date(DateFrom, DateTo) ->
     DateFrom2 = to_timestamp(DateFrom),
     DateTo2 = to_timestamp(DateTo),
     
-    %io:format("~n DateFrom: ~p; ~n", [DateFrom2]),
-    %io:format("~n DateTo: ~p; ~n", [DateTo2]),
-
     ?PID ! {lookup_by_date, self(), DateFrom2, DateTo2},
     
     get_response().
@@ -71,7 +57,6 @@ stop() ->
     ?PID ! {stop, self()}.
 
 get_response() ->
-    %io:format("~n get_response: ~n"),
     
 	receive
 		{Pid, Msg} ->
@@ -96,40 +81,26 @@ loop(Drop_Interval) ->
 	receive
         {start, Pid} -> 
             ets:new(?TABLE_NAME, [public, named_table]),
-            %io:format("~nets:new~n"),
             Pid ! {self(), start_done},
 			loop(Drop_Interval);
+
         {insert, Pid, Key, Value, Interval} -> 
-            %io:format("~nchild_insert(Key, Value, Interval):~n"),
-
             TimeExpire = get_timestamp() + Interval * 1000,
-            %io:format("~nTimeExpire: ~p~n", [TimeExpire]),
-
             ets:insert(?TABLE_NAME, {Key, Value, TimeExpire}),
-            %io:format("~nets:insert(table1, {~p, ~p, ~p}): ~n", [Key, Value, TimeExpire]),
-    
             Pid ! {self(), insert_done},
-
 			loop(Drop_Interval);
+
         {lookup, Pid, Key} ->
             Value = child_lookup(Key),
-            %io:format("~n Lookup: ~p~n", [Key]),
-
             Pid ! {self(), Value},
-            %io:format("~n Pid ! {self(), Value} ~n"),
-
             loop(Drop_Interval);
+
         {lookup_by_date, Pid, DateFrom, DateTo} -> 
             Res = ets:select(?TABLE_NAME, [{{'$1', '$2', '$3'}, [{'>=', '$3', DateFrom}, {'=<','$3',DateTo}], ['$$']}]),
-            
-            %Res = child_lookup_by_date(DateFrom, DateTo),
             Pid ! {self(), Res},
             loop(Drop_Interval);
 
         {lookup_all, Pid} ->
-            %io:format("~n lookup_all: ~n"),
-            %Res = child_lookup_all(),
-
             Res = ets:tab2list(?TABLE_NAME),
             Pid ! {self(), Res},
             loop(Drop_Interval);
@@ -139,9 +110,6 @@ loop(Drop_Interval) ->
             Pid ! {self(), delete_expired_done},
             loop(Drop_Interval);
 
-        %{delete} -> 
-        %    delete_expired(),
-        %   loop(Drop_Interval);
 		{stop, Pid} ->
             %io:format("~nStop~n"),
             Pid ! {self(), stop_done},
@@ -151,15 +119,13 @@ loop(Drop_Interval) ->
 child_lookup(Key) ->
     CurrentTime = get_timestamp(),
     Record = ets:lookup(table1, Key),
+
     case Record =:= [] of
         false -> 
             [{Key, Value, TimeExpire}] = Record,
 
-            %io:format("~n ~p >= ~p; ~n", [TimeExpire, CurrentTime]),
-
             case TimeExpire >= CurrentTime of 
                 true ->
-                    %io:format("~n Value: ~p; ~n", [Value]),
                     Value;
                 false ->
                     io:format("~n Time is out for current key ~n")
@@ -170,106 +136,9 @@ child_lookup(Key) ->
     end.
 
 
-%child_lookup_all() ->
-%        Res = ets:tab2list(?TABLE_NAME),
-%        Res.
-
-        %io:format("~n Res: ~p; ~n", [Res]).
-
-        %case Res of 
-        %[] ->
-        %    Res1 = ets:first(?TABLE_NAME),
-        %    io:format("~n Res1 = ets:first(?TABLE_NAME): ~n"),
-        %    Record = ets:lookup(?TABLE_NAME, Res1),
-        %    [{Key, Value, TimeExpire}] = Record,
-        %    io:format("~n ~p;~p;~p; ~n", [Key, Value, TimeExpire]),
-        %    child_lookup_all(Res1);
-        %"$end_of_table" ->
-        %    io:format("~n Res1 =:= '$end_of_table' ~n"),
-        %    Res1 = false;
-        %_ -> 
-        %    Res1 = ets:next(?TABLE_NAME, Res),
-        %    io:format("~n Res1 = ets:next(?TABLE_NAME, Res): ~n"),
-        %    Record = ets:lookup(?TABLE_NAME, Res1),
-        %    
-        %    case Record =/= [] of 
-        %        true ->
-        %            io:format("~n Record: ~p~n: ", [Record]),
-        %            [{Key, Value, TimeExpire}] = Record,
-        %            io:format("~n ~p;~p;~p; ~n", [Key, Value, TimeExpire]),
-        %            child_lookup_all(Res1);
-        %        false -> 
-        %            false
-        %    end
-    %end.
-
-
-
-%child_lookup_by_date(DateFrom, DateTo) ->
-%        io:format("~n child_lookup_by_date: ~p;~p;~n", [DateFrom, DateTo]),
-
-%        ets:select(?TABLE_NAME, [{{'$1', '$2', '$3'}, [{'>=', '$3', DateFrom}, {'=<','$3',DateTo}], ['$$']}]).
-        %io:format("~n Res: ~p; ~n", [Res]).
-
-        %case Res of 
-        %[] ->
-        %    Res1 = ets:first(?TABLE_NAME),
-        %    io:format("~n Res1 = ets:first(?TABLE_NAME): ~n"),
-        %    Record = ets:lookup(?TABLE_NAME, Res1),
-        %    [{Key, Value, TimeExpire}] = Record,
-        %    io:format("~n ~p;~p;~p; ~n", [Key, Value, TimeExpire]),
-        %    io:format("~n ~p >= ~p; ~p =< ~p; ~n", [TimeExpire, DateFrom, TimeExpire, DateTo]),
-
-        %    case ((TimeExpire >= DateFrom) and (TimeExpire =< DateTo)) of 
-        %        true ->
-        %            child_lookup_by_date(Res1, DateFrom, DateTo, [Acc|Record]);
-        %        false -> 
-        %            child_lookup_by_date(Res1, DateFrom, DateTo, [Acc])
-        %    end;
-        %"$end_of_table" ->
-        %    io:format("~n Res1 =:= '$end_of_table' ~n"),
-        %    %Res1 = false;
-        %    io:format("~nAcc2: ~p;~n", [Acc]),
-        %    Acc;
-        %_ -> 
-        %    Res1 = ets:next(?TABLE_NAME, Res),
-        %    io:format("~n Res1 = ets:next(?TABLE_NAME, Res): ~n"),
-        %    Record = ets:lookup(?TABLE_NAME, Res1),
-        %    
-        %    case Record =/= [] of 
-        %        true ->
-        %            io:format("~n Record: ~p~n: ", [Record]),
-        %            [{Key, Value, TimeExpire}] = Record,
-        %            io:format("~n true ~p;~p;~p; ~n", [Key, Value, TimeExpire]),
-        %            io:format("~n ~p >= ~p; ~p =< ~p; ~n", [TimeExpire, DateFrom, TimeExpire, DateTo]),
-
-
-        %            case ((TimeExpire >= DateFrom) and (TimeExpire =< DateTo)) of 
-        %                true ->
-        %                    io:format("~n true ~n"),
-        %                    child_lookup_by_date(Res1, DateFrom, DateTo, [Acc|Record]);
-        %                false -> 
-        %                    io:format("~n false ~n"),
-
-        %                    child_lookup_by_date(Res1, DateFrom, DateTo, [Acc])
-        %            end;
-
-        %            %child_lookup_by_date(Res1, DateFrom, DateTo, [Acc|Record]);
-        %        false -> 
-        %            io:format("~nfalse Acc2: ~p;~n", [Acc]),
-        %            Acc
-        %    end
-
-
-    %end.
-
-
-
 child_delete_expired() ->
-        %io:format("~n child_delete_expired Res: ~n", []),
-
-        CurrentTime = get_timestamp(),
-        ets:select_delete(?TABLE_NAME, [{{'$1', '$2', '$3'}, [{'<', '$3', CurrentTime}], [true]}]).
+    CurrentTime = get_timestamp(),
+    ets:select_delete(?TABLE_NAME, [{{'$1', '$2', '$3'}, [{'<', '$3', CurrentTime}], [true]}]).
 
 
 get_timestamp() ->                                                                                                                                                                                   
