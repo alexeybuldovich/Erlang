4854db7d6d11a5b92b93341e72aeee83d9b14b3e
Алексей Бульдович <a.buldovich@betinvest.com>
2018-04-03 14:18:57 +0300

    Lesson 7

---
+++ webserver/make_project.sh
@@ -0,0 +1,5 @@
+#bin/bash
+
+wget https://erlang.mk/erlang.mk
+make -f erlang.mk bootstrap-lib
+make
---
+++ webserver/src/toppage_handler.erl
@@ -0,0 +1,24 @@
+%% Feel free to use, reuse and abuse the code in this file.
+
+%% @doc Hello world handler.
+-module(toppage_handler).
+
+-export([init/3]).
+-export([handle/2]).
+-export([terminate/3]).
+
+init(_Type, Req, []) ->
+	{ok, Req, undefined}.
+
+handle(Req, State) ->
+    Res_json = jsx:encode([{<<"library">>,<<"jsx">>},{<<"awesome">>,true}]),
+    
+
+	{ok, Req2} = cowboy_req:reply(200, [
+		{<<"content-type">>, <<"text/plain">>}
+	], Res_json, Req),
+	{ok, Req2, State}.
+
+terminate(_Reason, _Req, _State) ->
+	ok.
+
